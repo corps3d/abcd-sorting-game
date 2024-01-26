38f073b118181805e91520d7b1f0a6753f649944
Hamid Ali Khan <sp21-bcs-009@cuiatk.edu.pk>
2024-01-26 15:57:27 +0500

    dynamically add random selected alphbets, ensures random order

--- sort.js
+++ sort.js
@@ -1,50 +1,31 @@
 $(document).ready(function() {
-    // Hide all blocks
-    $(".block").hide();
-
+    alphabets = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    
     $("#activateBlocks").on("click", function() {
-        $(".block").removeClass("active");
-
+        $('.block').remove();
+        // Randomly select three block divs
         var selectedIndexes = [];
         while (selectedIndexes.length < 3) {
             var randomIndex = getRandomIndex();
-            console.log(randomIndex);
 
-            // Ensure the selected index is unique
             if (!selectedIndexes.includes(randomIndex)) {
                 selectedIndexes.push(randomIndex);
-                $(".block").eq(randomIndex).addClass("active");
             }
         }
 
-        // Shuffle the array
-        selectedIndexes = shuffle(selectedIndexes);
-
-        selectedIndexes.forEach(function(index) {
-            $(".block.active").eq(index).show();
-        });
+        addActiveBlocks(selectedIndexes);
     });
 
-    function getRandomIndex() {
-        return Math.floor(Math.random() * 26); 
+    // Function to add only the selected and shuffled blocks
+    function addActiveBlocks(indexes) {
+        for (var i = 0; i < indexes.length; i++) {
+            console.log(indexes);
+            $('#blocksContainer').append('<div class="block active">' + alphabets[indexes[i]] + '</div>')
+        }
+        $('.active').show();
     }
 
-    // Function to shuffle
-    function shuffle(array) {
-        var m = array.length, t, i;
-      
-        // While there remain elements to shuffle…
-        while (m) {
-      
-          // Pick a remaining element…
-          i = Math.floor(Math.random() * m--);
-      
-          // And swap it with the current element.
-          t = array[m];
-          array[m] = array[i];
-          array[i] = t;
-        }
-        console.log(array);
-        return array;
+    function getRandomIndex() {
+        return Math.floor(Math.random() * 26);
     }
 });
